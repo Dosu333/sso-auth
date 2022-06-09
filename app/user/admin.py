6c1cd8900e2471ec3bb0af566908dbe2bc075747
Dosu Larinde <vladamir1865@gmail.com>
2022-06-09 16:10:30 +0000

    phone

--- app/user/admin.py
+++ app/user/admin.py
@@ -5,26 +5,31 @@ from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from .models import User, Token
 
+
 class UserResource(resources.ModelResource):
     class Meta:
         model = User
-        fields = ('id', 'firstname', 'lastname', 'email', 'phone', 'verified', 'password')
+        fields = ('id', 'firstname', 'lastname', 'email', 'phone', 'verified', 'password', 'roles', 'is_active',
+                  'is_staff', 'is_superuser', 'referred_by', 'referred_by_hero', 'referred_by_store_owner')
+
 
 class UserAdmin(BaseUserAdmin, ImportExportModelAdmin):
-    resource_class =UserResource
-    
+    resource_class = UserResource
+
     ordering = ['email']
-    list_display = ['email', 'lastname', 'firstname', 'roles', 'verified', 'created_at']
+    list_display = ['email', 'lastname', 'firstname',
+                    'roles', 'verified', 'created_at']
     list_filter = ['is_active', 'is_staff', 'verified']
     search_fields = ('id', 'email', 'lastname', 'firstname', 'phone', 'roles')
     date_hierarchy = 'created_at'
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {
-            'fields': ('lastname', 'firstname', 'phone', 'roles', 'image','expensive_rate')}),
+            'fields': ('lastname', 'firstname', 'phone', 'roles', 'image', 'expensive_rate')}),
         (
             _('Permissions'),
-            {'fields': ('is_active', 'is_staff', 'is_superuser', 'verified', 'referred_by', 'referred_by_hero', 'referred_by_store_owner')}
+            {'fields': ('is_active', 'is_staff', 'is_superuser', 'verified',
+                        'referred_by', 'referred_by_hero', 'referred_by_store_owner')}
         ),
         (_('Important Info'), {'fields': ('last_login',)})
     )
@@ -34,6 +39,7 @@ class UserAdmin(BaseUserAdmin, ImportExportModelAdmin):
             'fields': ('email', 'lastname', 'firstname', 'roles', 'verified', 'password1', 'password2')
         }),
     )
+
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Token)
