970d7739a534880326bc604580b94d40fd0311d1
Dosu Larinde <vladamir1865@gmail.com>
2022-08-02 21:29:22 +0000

    admin marketplace notification celery task

--- app/user/tasks.py
+++ app/user/tasks.py
@@ -5,6 +5,9 @@ from django.template.loader import get_template
 from django.core.management import call_command
 from .utils import send_email
 from core.celery import APP
+from twilio.rest import Client
+from datetime import datetime
+import requests
 
 
 @APP.task()
@@ -37,3 +40,20 @@ def send_password_reset_email(email_data):
     text_alternative = text_template.render(email_data)
     send_email('Password Reset',
                email_data['email'], html_alternative, text_alternative)
+
+
+@APP.task()
+def admin_marketplace_notify():
+    client = Client()
+    admin_to_numbers = ['+2347056918098', '+2348136800327', '+2349077499434']
+    url = "http://3.14.60.165:6080/api/v1/admin/droppers"
+    res = requests.get(url, verify=False)
+    response = res.json()
+    now = datetime.now()
+    
+    for order in response['orders']:
+        if str(now.date()) == order['date'] and datetime.fromtimestamp(float(order['timestamp'])).time().minute == now.time().minute:
+            items = []
+            body = f"{order['customer_name']} just ordered a meal from {order['store_name']}. The meal is to be delivered to {order['customer_location']}. You can reach {order['customer_name']} via this phone number {order['customer_phone_number']}. This order is {order['status']}. For more info on this order, check the app."
+            for number in admin_to_numbers:
+                client.messages.create(from_='+12312625574', to=number, body=body)
